b9fefbb8ae8cbd178bc457692b461cb9de979fa4
Mateusz Baginski <cziken58@gmail.com>
2022-12-04 14:39:51 +0100

    fix: remove eslint errors in rollup.shared.config.ts

--- config/rollup.shared.config.ts
+++ config/rollup.shared.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type, import/no-extraneous-dependencies */
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 
@@ -5,13 +6,15 @@ export const createPackageRollupConfig = () => ({
   input: 'src/index.ts',
   output: {
     file: 'dist/index.js',
-    format: 'cjs'
+    format: 'cjs',
   },
   plugins: [
-    typescript(),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
     resolve({
-      moduleDirectories: ['node_modules']
-    })
+      moduleDirectories: ['node_modules'],
+    }),
   ],
-  external: ['react']
+  external: ['react'],
 });
