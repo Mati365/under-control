282bd56dc15fa0be9947d048ac27da7c37dc8872
Mateusz Baginski <cziken58@gmail.com>
2022-12-04 14:02:11 +0100

    feat: add prettier / rollup config

--- packages/forms/rollup.config.ts
+++ packages/forms/rollup.config.ts
@@ -1,3 +1,4 @@
-import { createPackageRollupConfig } from "../../config/rollup.shared.config";
+/* eslint-disable import/no-default-export */
+import { createPackageRollupConfig } from '../../config/rollup.shared.config';
 
 export default createPackageRollupConfig();
