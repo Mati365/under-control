9b350343597d4c5cc58d7a3ad3c10824e5af4f8b
Mateusz Baginski <cziken58@gmail.com>
2022-12-11 18:18:36 +0100

    feat: improve compiler

--- config/rollup.shared.config.mjs
+++ config/rollup.shared.config.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import del from 'rollup-plugin-delete';
 import { externals } from 'rollup-plugin-node-externals';
@@ -20,7 +20,13 @@ export const createPackageRollupConfig = ({ outDir = 'dist' } = {}) => ({
     }),
     typescript({
       tsconfig: './tsconfig.json',
-      declarationDir: './',
+      tsconfigOverride: {
+        compilerOptions: {
+          module: 'ESNext',
+          declaration: true,
+        },
+        exclude: ['node_modules/', '**/*.test.tsx', '**/*.test.ts'],
+      },
     }),
     externals(),
     resolve({
